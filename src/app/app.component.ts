0cfe09fd7f7334d977dd1d96c9aa9a162063705f
yhb <yhb@xlbzone.com>
2018-07-06 15:57:36 +0800

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform, ToastController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
 import { ApiProvider } from '../core/Api';
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage: any = HomePage;
+    rootPage: any = TabsPage;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, apiProvider: ApiProvider) {        
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, apiProvider: ApiProvider, private toastCtrl: ToastController) {
         platform.ready().then(async () => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.            
@@ -23,7 +22,12 @@ export class MyApp {
                 let test = await apiProvider.test();
                 console.log(test);
             } catch (e) {
-
+                let toast = toastCtrl.create({
+                    message: 'just a test:' + e.message,
+                    duration: 3000,
+                    position: 'top'
+                });
+                toast.present();
             }
         });
     }
--- src/core/Api.ts
+++ src/core/Api.ts
@@ -4,13 +4,16 @@ import { Http, Response } from '@angular/http';
 export class ApiProvider {
     readonly host: string;
     constructor(private http: Http) {
-        this.host = 'http://localhost:5501';
+        this.host = 'http://your.host';
     }
 
     private send(url: string, method: string = 'post', data?: any) {
         return this.http.request(this.host + url, { method: method, body: data }).toPromise().then((t: Response) => {
             let result = t.json();
             return result;
+        }).catch((e) => {
+            let msg = e.statusText || e.message || 'request error';            
+            throw new Error(msg);
         });
     }
 
@@ -23,6 +26,6 @@ export class ApiProvider {
     }
 
     test() {
-        return this.get('/api/fleet/captcha');
+        return this.get('');
     }
 }   
